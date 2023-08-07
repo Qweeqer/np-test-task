6277c1e8714e3080d97a4f12bbdb0345fcd82bc3
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-07 20:52:12 +0300

    Add  component OfficeSearch

---
+++ src/components/OfficeSearch/OfficeSearch.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getCities, getWarehouses } from '../../redux/offices/operations';
+import CitySelector from '../CitySelector/CitySelector';
+import OfficeList from '../OfficeList/OfficeList';
+import { selectCity } from '../../redux/offices/selectors';
+
+import { Container, Text } from './OfficeSearch.styled';
+
+const OfficeSearch = () => {
+  const [cityName, setCityName] = useState('');
+
+  const dispatch = useDispatch();
+  const city = useSelector(selectCity);
+
+  useEffect(() => {
+    dispatch(getCities());
+    if (cityName) {
+      dispatch(getWarehouses(cityName));
+    }
+  }, [dispatch, cityName]);
+
+  const getCityName = city => {
+    setCityName(city);
+  };
+
+  return (
+    <Container>
+      <CitySelector getCityName={getCityName} />
+      <Text>{city}</Text>
+      <OfficeList />
+    </Container>
+  );
+};
+
+export default OfficeSearch;
